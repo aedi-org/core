33d2597a03eee40dd172871adf8c7609e76f5636
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-10-21 18:10:24 +0300

    aedi: remove absolute paths workaround from pkgconf
    it becomes obsolete when DESTDIR installation was enabled, 231e436e846368a9772b80ccfa2dce3d919a83c0

--- aedi/target/tool.py
+++ aedi/target/tool.py
@@ -136,19 +136,6 @@ class PkgconfTarget(base.ConfigureMakeStaticDependencyTarget):
     def detect(self, state: BuildState) -> bool:
         return state.has_source_file('libpkgconf/libpkgconf.h')
 
-    def configure(self, state: BuildState):
-        hardcoded_prefix = '/usr/local'
-        hardcoded_libdir = hardcoded_prefix + '/lib'
-        hardcoded_pkgdir = hardcoded_libdir + '/pkgconfig'
-
-        opts = state.options
-        opts['--with-personality-dir'] = hardcoded_pkgdir + '/personality.d'
-        opts['--with-pkg-config-dir'] = hardcoded_pkgdir
-        opts['--with-system-includedir'] = hardcoded_prefix + '/include'
-        opts['--with-system-libdir'] = hardcoded_libdir
-
-        super().configure(state)
-
     def post_build(self, state: BuildState):
         self.copy_to_bin(state)
 
